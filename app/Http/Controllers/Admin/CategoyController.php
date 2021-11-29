f5d54856d3df3a3cecd96c9432a926e956e03822
Noir <anhlq1209@gmail.com>
2021-11-29 23:38:44 +0700

    update controller

--- app/Http/Controllers/Admin/CategoyController.php
+++ app/Http/Controllers/Admin/CategoyController.php
@@ -3,16 +3,28 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\CategoryFormRequest;
+use App\Http\Requests\Auth\RegisterFormRequest;
+use App\Http\Services\Shop\CategoryService;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CategoyController extends Controller
 {
+    protected $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
     public function index()
     {
+        $categories = Category::all();
         return view('admin.category.index', [
             'title' => 'Categories',
-            'categories' => $this->categories
+            'categories' => $categories
         ]);
     }
 
@@ -22,8 +34,13 @@ class CategoyController extends Controller
         ]);
     }
 
-    public function store(Request $request) {
-        
+    public function store(CategoryFormRequest $request) {
+        if ($this->categoryService->create($request)) {
+            return redirect()->back();
+        }
+
+        Session::flash('error', 'Vui lòng nhập đủ thông tin Category');
+        return redirect()->back()->withInput();
     }
 
     public function edit($id) {
