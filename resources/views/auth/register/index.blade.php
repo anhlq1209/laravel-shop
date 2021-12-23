39ca3c5fbaea7c4f80fdc2b0d83dcb53962c0136
Noir <anhlq1209@gmail.com>
2021-12-23 08:59:51 +0700

    update layout 1

--- resources/views/auth/register/index.blade.php
+++ resources/views/auth/register/index.blade.php
@@ -14,60 +14,52 @@
 
             <div class="card">
                 <div class="card-body register-card-body">
-                <p class="login-box-msg">Register a new membership</p>
+                    <p class="login-box-msg">Register a new membership</p>
 
-                @include('layouts.alert')
+                    @include('layouts.alert')
 
-                <form action="/register/store" method="post">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" name="name" placeholder="Full name">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-user"></span>
+                    <form action="/register/store" method="post">
+                        @csrf
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" name="name" placeholder="Full name">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-user"></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="email" class="form-control" name="email" placeholder="Email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
+                        <div class="input-group mb-3">
+                            <input type="email" class="form-control" name="email" placeholder="Email">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" name="password" placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" name="password" placeholder="Password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" name="passwordRetype" placeholder="Retype password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" name="passwordRetype" placeholder="Retype password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-                                <label for="agreeTerms">
-                                    I agree to the <a href="#">terms</a>
-                                </label>
+                        <div class="row">
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-primary btn-block">Register</button>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Register</button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
 
-                <a href="/login" class="text-center">I already have a membership</a>
+                    <a href="/login" class="text-center">I already have a membership</a>
                 </div>
             </div>
         </div>
---
+++ resources/views/verify-email.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h4>Chào {{ $data['name'] }}</h4>
+    <p>Noir Lee SHOP xin gửi đến bạn đường dẫn xác nhận tài khoản: {{ $data['link'] }}</p>
+ </div>
+ 
